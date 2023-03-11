3c58e2940729ba68c144badbbca1d639d550dffe
Benson Liu <epiccowslayer@gmail.com>
2023-03-11 16:36:11 -0500

    Added spotify API call to get songs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
+import axios from 'axios';
 
-function App() {
+let songs = [];
+const playlistIDS = ['40Vb0BdWCuGQzcAqKzYf5L', '5VgkKiWwUIhRJDFxg48RA1', '65JdYHLTHyGMzc3LaQ0BsY']
+
+for (const id of playlistIDS) {
+  const options = {
+    method: 'GET',
+    url: 'https://spotify23.p.rapidapi.com/playlist_tracks/',
+    params: { id: id, offset: '0', limit: '100' },
+    headers: {
+      'X-RapidAPI-Key': 'f8e079566emsh44b588e8f93db6fp197ec8jsn083a3171aa4c',
+      'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+    songs = songs.concat(response.data.items);
+    console.log(songs);
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+
+const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <p>
-        hello world
+        hello world!
       </p>
     </div>
-  );
+  )
 }
 
 export default App;
